503e2d6c52c499d2417fb93ffd19ee5ee9417e2f
Hernan Bou <hernan_bou@hotmail.com>
2025-01-07 18:44:41 -0300

    First Commit

---
+++ src/pages/MapSelector.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface Map {
+  id: number;
+  owner: string;
+  name: string;
+  image: string;
+}
+
+interface MapSelectorProps {
+  maps: Map[];
+  onSelectMap: (mapId: number) => void;
+}
+
+const MapSelector: React.FC<MapSelectorProps> = ({ maps, onSelectMap }) => {
+  return (
+    <div>
+      <h2>Selecione um mapa:</h2>
+      <ul>
+        {maps.map((map) => (
+          <li key={map.id}>
+            <button onClick={() => onSelectMap(map.id)}>
+              {map.name} - {map.owner}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MapSelector;
